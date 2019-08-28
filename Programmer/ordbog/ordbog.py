2cf3e6cc01b9d4978608f08ac79e5afef8fd6e4a
Hoffmann <45014509+HoffmannDev@users.noreply.github.com>
2019-08-28 15:29:39 +0200

    fixe gui
    Fixed problem where gui would no launch

---
+++ Programmer/ordbog/ordbog.py
@@ -0,0 +1,12 @@
+import wx
+import gui
+
+class mainFrame(gui.mainFrame):
+    def __init__(self, parent):
+        gui.mainFrame.__init__(self, parent)
+
+app =wx.App(False)
+frame = mainFrame(None)
+frame.Show(True)
+app.MainLoop()
+        
