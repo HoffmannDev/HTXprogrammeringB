6bd50ebffd1d7667596f3d3e05bbe06890d9bb05
Hoffmann <45014509+HoffmannDev@users.noreply.github.com>
2019-09-10 12:46:36 +0200

    idk bro

--- Programmer/lan program/gui.py
+++ Programmer/lan program/gui.py
@@ -10,3 +10,72 @@
 import wx
 import wx.xrc
 
+###########################################################################
+## Class mainFrame
+###########################################################################
+
+class mainFrame ( wx.Frame ):
+
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+		bSizer1 = wx.BoxSizer( wx.VERTICAL )
+
+		bSizer2 = wx.BoxSizer( wx.HORIZONTAL )
+
+
+		bSizer2.Add( ( 0, 0), 1, wx.EXPAND, 5 )
+
+		self.orderBtn = wx.Button( self, wx.ID_ANY, u"Order", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer2.Add( self.orderBtn, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
+
+
+		bSizer2.Add( ( 0, 0), 1, wx.EXPAND, 5 )
+
+		self.seeOrderBtn = wx.Button( self, wx.ID_ANY, u"See Order", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer2.Add( self.seeOrderBtn, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+
+
+		bSizer2.Add( ( 0, 0), 1, wx.EXPAND, 5 )
+
+
+		bSizer1.Add( bSizer2, 1, wx.EXPAND|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+
+
+		self.SetSizer( bSizer1 )
+		self.Layout()
+
+		self.Centre( wx.BOTH )
+
+		# Connect Events
+		self.orderBtn.Bind( wx.EVT_BUTTON, self.openOrder )
+
+	def __del__( self ):
+		pass
+
+
+	# Virtual event handlers, overide them in your derived class
+	def openOrder( self, event ):
+		event.Skip()
+
+
+###########################################################################
+## Class orderWindow
+###########################################################################
+
+class orderWindow ( wx.Frame ):
+
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Order", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+
+		self.Centre( wx.BOTH )
+
+	def __del__( self ):
+		pass
+
+
--- Programmer/lan program/main.py
+++ Programmer/lan program/main.py
@@ -1,3 +1,13 @@
 import wx
 import gui
 import pickle
+
+class mainFrame(gui.mainFrame):
+	def __init__(self, parent):
+		gui.mainFrame.__init__(self, parent)
+
+
+app = wx.App(False)
+frame = mainFrame(None)
+frame.Show(True)
+app.MainLoop() 
