ee9ac94bb30b48b45a095ae4880512b0d411876c
Hoffmann <45014509+HoffmannDev@users.noreply.github.com>
2019-10-30 12:13:12 +0100

    done, it work now

--- Programmer/lan program/main.py
+++ Programmer/lan program/main.py
@@ -1,32 +1,71 @@
 import wx
 import gui
-import sql
-
+import sqlite3 as lite
 con = None
-con = lite.connect('deltagere.db')
+con = lite.connect('lanparty.db')
 cur = con.cursor()
 
-class mainFrame(gui.mainFrame):
-	def __init__(self, parent):
-		gui.mainFrame.__init__(self, parent)
-		self.orderWindow = orderWindow(self)
+class gui_wx(gui.MainFrame, gui.LogInDialog, gui.BordDialog, gui.DeltagerDialog):
+    def __init__(self, parent):
+        gui.MainFrame.__init__(self, parent)
+        sql ="SELECT id, fnavn, enavn, klasse, bord FROM deltagere"
+        cur.execute(sql)
+        rows = cur.fetchall()
+        for row in rows:
+            self.listbox_deltagere.Append(str(row[0])+" "+row[1]+", "+row[2]+", "+row[3] + "," + row[4])
 
-	def openOrder(self, event):
-		self.orderWindow.Show()
+    def afslut( self, event ):
+        exit(0)
 
-class orderWindow(gui.orderWindow):
-	def __init__(self, parent):
-		gui.orderWindow.__init__(self, parent)
+    def vaelgbord( self, event ):
+        dlg = gui.BordDialog(self)
+        res = dlg.ShowModal()
+        if res == wx.ID_OK:
+            valg = dlg.bordvalg.GetSelection()
+            if valg == 0:
+                print("Du har valgt stort bord")
+            else:
+                print("Du har valgt lille bord")
+        dlg.Destroy()
 
-	def placeOrder(self, event):
-		sql = "INSER INTO deltagere (fnavn) VALUES("
-		sql += self.fnavn_felt.GetValue() + ")"
-		cur.execute(sql)
-		cur.commit()
-		
+    def login( self, event ):
+        dlg = gui.LogInDialog(self)
+        res = dlg.ShowModal()
+        if res == wx.ID_OK:
+            print(dlg.username.GetValue())
+            print(dlg.pword.GetValue())
+            self.vaelg_bord.Enable(True)
 
+        if res == wx.ID_CANCEL:
+            print("Cancel")
+
+    def vis_deltager( self, event ):
+        valg = self.listbox_deltagere.GetStringSelection()
+        nr = valg[0]
+        print(nr)
+
+    def ny_deltager( self, event ):
+        dlg = gui.DeltagerDialog(self)
+        res = dlg.ShowModal()
+        if res == wx.ID_OK:
+            fnvn = dlg.fnavn.GetValue()
+            envn = dlg.enavn.GetValue()
+            klasse = dlg.klasse.GetValue()
+            valg = dlg.bordvalg.GetSelection()
+            if valg == 0:
+                vlg = 0
+            else:
+                vlg = 1
+            sql ="INSERT INTO deltagere (fnavn, enavn, klasse, bord) VALUES ("
+            sql += "'" + fnvn + "'"
+            sql += ", '" + envn + "'"
+            sql += ", '"+klasse+"'"
+            sql += ", '"+str(vlg)+"'"
+            sql += ")"
+            cur.execute(sql)
+            con.commit()
 
 app = wx.App(False)
-frame = mainFrame(None)
+frame = gui_wx(None)
 frame.Show(True)
 app.MainLoop()
